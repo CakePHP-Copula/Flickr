fc25ed9256ff15e6f1ed66b8bc78f6da3affd3c3
Dean Sofer <proloser@hotmail.com>
2011-04-12 23:47:09 -0700

    Removed unused code

--- models/datasources/apis/flickr.php
+++ models/datasources/apis/flickr.php
@@ -8,15 +8,6 @@
  * @author Dean Sofer
  **/
 class Flickr extends ApisSource {
-
-	/**
-	 * Array containing the names of components this component uses. Component names
-	 * should not contain the "Component" portion of the classname.
-	 *
-	 * @var array
-	 * @access public
-	 */
-	var $config = array();
 	
 	// TODO: Relocate to a dedicated schema file
 	var $_schema = array();
@@ -34,50 +25,4 @@ class Flickr extends ApisSource {
     );
     
     protected $url = ':protocol://api.flickr.com/services/rest/?api_key=:login&method=:path&format=:format';
-	
-
-/**
- * Used to create new records. The "C" CRUD.
- *
- * To-be-overridden in subclasses.
- *
- * @param Model $model The Model to be created.
- * @param array $fields An Array of fields to be saved.
- * @param array $values An Array of values to save.
- * @return boolean success
- * @access public
- */
-	function create(&$model, $fields = null, $values = null) {
-		return false;
-	}
-
-/**
- * Update a record(s) in the datasource.
- *
- * To-be-overridden in subclasses.
- *
- * @param Model $model Instance of the model class being updated
- * @param array $fields Array of fields to be updated
- * @param array $values Array of values to be update $fields to.
- * @return boolean Success
- * @access public
- */
-	function update(&$model, $fields = null, $values = null) {
-		return false;
-	}
-
-/**
- * Delete a record(s) in the datasource.
- *
- * To-be-overridden in subclasses.
- *
- * @param Model $model The model class having record(s) deleted
- * @param mixed $id Primary key of the model
- * @access public
- */
-	function delete(&$model, $id = null) {
-		if ($id == null) {
-			$id = $model->id;
-		}
-	}
 }
